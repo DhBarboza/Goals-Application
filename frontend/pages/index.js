909133468cfb97171e6fe968759eabd3f0241e5a
Douglas Henrique <dev.dhbarboza@gmail.com>
2021-01-08 20:06:39 -0300

    Upgrade page add components and layout

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,15 +1,47 @@
+import React from 'react';
+import { Jumbotron, Container, ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText } from 'reactstrap';
+
+// Import Componente:
+import Menu from '../components/Navbar';
+
+
 function HomePage({ data }) {
     return (
         <>
-            <h1>Minhas Metas!</h1>
-            {data.metas.map(meta => (
-                <div key="{meta.id}">
-                    <h2>{meta.name}</h2>
-                    <p>{meta.description}</p>
-                    <p>{meta.status}</p>
-                    <hr />
-                </div>
-            ))}
+            <Menu />
+            <Jumbotron fluid className="list">
+                <style>
+                    {
+                        `.list {
+                            background-color: #4169E1;
+                            padding-top: 30px;
+                            padding-bottom: 157px;
+                            margin-bottom: 0rem !important;
+                        }
+                        .title-top {
+                            color: #7FFFD4;
+                        }
+                        .list-meta {
+                            background-color: #4169E1 !important;
+                            border: .2rem solid #FFF !important;
+                        }`
+                    }
+                </style>
+                <Container>
+                    <h1 className="display-4 text-center title-top" >Minhas Metas!</h1>
+                    <ListGroup>
+                        {data.metas.map(meta => (
+                            <div key={meta._id}>
+                                <ListGroupItem className="list-meta" active>
+                                    <ListGroupItemHeading>{meta.name}</ListGroupItemHeading>
+                                    <ListGroupItemText>{meta.descripition}</ListGroupItemText>
+                                    <ListGroupItemText>{meta.status}</ListGroupItemText>
+                                </ListGroupItem>
+                            </div>
+                        ))}
+                    </ListGroup>
+                </Container>
+            </Jumbotron>
         </>
     );
 }
